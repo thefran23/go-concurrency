ee373dcee4a46cd29ca7c27e2bc7fee0a57f86b3
Francois <francois@innostrem.co>
2024-07-19 15:06:46 +0200

    wip

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,64 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 const webPort = "80"
 
 func main() {
+	db := initDB()
+	db.Ping()
 
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Panic("cant connect to db")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgres not yet ready")
+			counts++
+		} else {
+			log.Println("connected to database")
+			return connection
+		}
+		if counts > 10 {
+			return nil
+		}
+
+		log.Print("Backing off for 1 second")
+		time.Sleep(1 * time.Second)
+	}
+
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
