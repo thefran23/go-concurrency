636ef3e91c0a9226b6b1376c86ee1393d1464f58
Francois <francois@innostrem.co>
2024-07-22 11:08:34 +0200

    wip

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/alexedwards/scs/redisstore"
-	"github.com/alexedwards/scs/stores/redisstore"
 	"github.com/alexedwards/scs/v2"
 	"github.com/gomodule/redigo/redis"
 	_ "github.com/jackc/pgconn"
@@ -20,14 +21,36 @@ const webPort = "80"
 
 func main() {
 	db := initDB()
-	db.Ping()
 	session := initSession()
+	wg := sync.WaitGroup{}
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := Config{
+		Session:  session,
+		DB:       db,
+		Wait:     &wg,
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+	}
+
+	app.serve()
+}
+
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+	app.InfoLog.Println("Starting web server...")
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic("Error starting web server:", err)
+	}
 }
 
 func initDB() *sql.DB {
 	conn := connectToDB()
-
 	if conn == nil {
 		log.Panic("cant connect to db")
 	}
@@ -49,11 +72,9 @@ func connectToDB() *sql.DB {
 		if counts > 10 {
 			return nil
 		}
-
 		log.Print("Backing off for 1 second")
 		time.Sleep(1 * time.Second)
 	}
-
 }
 
 func openDB(dsn string) (*sql.DB, error) {
@@ -65,19 +86,17 @@ func openDB(dsn string) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return db, nil
 }
 
 func initSession() *scs.SessionManager {
-session := scs.New()
-session.Store = redisstore.New(initRedis())
-
-session.Lifetime = 24 * time.Hour
-session.Cookie.Persist = true
-session.Cookie.SameSite = http.SameSiteLaxMode
-session.Cookie.Secure = true
-return session
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+	return session
 }
 
 func initRedis() *redis.Pool {
@@ -86,7 +105,6 @@ func initRedis() *redis.Pool {
 		Dial: func() (redis.Conn, error) {
 			return redis.Dial("tcp", os.Getenv("REDIS"))
 		},
-
 	}
 	return redisPool
 
