07105fb9582eada62275eb02c5d47ae5ba610eeb
Francois <francois@innostrem.co>
2024-07-22 14:43:47 +0200

    wip

---
+++ cmd/web/render.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+var pathToTemplates = "./cmd/web/templates/"
+
+type TemplateData struct {
+	StringMap     map[string]string
+	IntMap        map[string]int
+	FloatMap      map[string]float64
+	Data          map[string]any
+	Flash         string
+	Warning       string
+	Error         string
+	Authenticated bool
+	Now           time.Time
+	//User *date.User
+}
+
+func (app *Config) render(w http.ResponseWriter, r *http.Request, t string, td *TemplateData) {
+	partials := []string{
+		fmt.Sprintf("%s/base.layout.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/header.partial.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/navbar.partial.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/footer.partial.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/alerts.partial.gohtml", pathToTemplates),
+	}
+
+	var templates []string
+	templates = append(templates, fmt.Sprintf("%s/%s", pathToTemplates, t))
+
+	for _, x := range partials {
+		templates = append(templates, x)
+	}
+
+	if td == nil {
+		td = &TemplateData{}
+	}
+
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		app.ErrorLog.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, app.addDefaultData(td, r)); err != nil {
+		app.ErrorLog.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (app *Config) addDefaultData(td *TemplateData, r *http.Request) *TemplateData {
+	td.Flash = app.Session.PopString(r.Context(), "flash")
+	td.Warning = app.Session.PopString(r.Context(), "warning")
+	td.Error = app.Session.PopString(r.Context(), "error")
+	td.Authenticated = app.IsAuthenticated(r)
+	td.Now = time.Now()
+	return td
+}
+
+func (app *Config) IsAuthenticated(r *http.Request) bool {
+	return app.Session.Exists(r.Context(), "userID")
+
+}
