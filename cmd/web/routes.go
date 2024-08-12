9bd914e93e093611c43a312da897837e54a9ed15
Francois <francois@innostrem.co>
2024-08-12 14:27:55 +0200

    wip

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
@@ -17,6 +18,29 @@ func (app *Config) routes() http.Handler {
 	mux.Get("/register", app.RegisterPage)
 	mux.Post("/register", app.PostRegisterPage)
 	mux.Post("/activate-account", app.ActivateAccount)
+
+	mux.Get("/test-email", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("HERE................")
+
+		errchan := make(chan error)
+		m := Mail{
+			Domain:      "localhost",
+			Host:        "localhost",
+			Port:        1025,
+			Encryption:  "none",
+			FromAddress: "info@mycompany.com",
+			FromName:    "info",
+			ErrorChan:   errchan,
+		}
+
+		msg := Message{
+			To:      "me@here.com",
+			Subject: "Test mail",
+			Data:    "Hello, world",
+		}
+
+		m.sendMail(msg, make(chan error))
+	})
 
 	return mux
 }
